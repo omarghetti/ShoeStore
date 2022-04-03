0a8caf858d49a76c1962886e6ebed38f2a0a6a37
Omar Ghetti <omarghetti2@gmail.com>
2022-04-03 17:44:40 +0200

    Added ShoeListViewModel.kt, ShoeListFragment.kt + Started Development on ShoeDetailsFragment.kt

--- app/src/main/java/com/example/shoestore/ui/ShoeListFragment.kt
+++ app/src/main/java/com/example/shoestore/ui/ShoeListFragment.kt
@@ -1,21 +1,26 @@
 package com.example.shoestore.ui
 
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.*
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.shoestore.R
 import com.example.shoestore.databinding.FragmentShoeListBinding
 import com.example.shoestore.viewmodels.ShoeListViewModel
+import org.w3c.dom.Text
 
 class ShoeListFragment : Fragment() {
 
     private lateinit var binding: FragmentShoeListBinding
 
-    private lateinit var shoeListViewModel: ShoeListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +28,18 @@ class ShoeListFragment : Fragment() {
     ): View {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
-        shoeListViewModel = ViewModelProvider(this)[ShoeListViewModel::class.java]
+        val shoeListViewModel: ShoeListViewModel by activityViewModels()
 
         binding.shoeListViewModel = shoeListViewModel
         binding.setLifecycleOwner { viewLifecycleOwner.lifecycle }
+
+        shoeListViewModel.shoeList.observe(viewLifecycleOwner, Observer { shoeList ->
+            shoeList.forEachIndexed { index,shoe ->
+                val textView = TextView(context)
+                textView.setText("${shoe.name}, Size: ${shoe.size}, Description: ${shoe.description}")
+                binding.shoeListLinear.addView(textView,index)
+            }
+        })
         return binding.root
     }
 
--- app/src/main/java/com/example/shoestore/viewmodels/ShoeListViewModel.kt
+++ app/src/main/java/com/example/shoestore/viewmodels/ShoeListViewModel.kt
@@ -1,5 +1,27 @@
 package com.example.shoestore.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.shoestore.models.Shoe
 
-class ShoeListViewModel: ViewModel()
+class ShoeListViewModel: ViewModel() {
+    private var _shoeList = MutableLiveData<MutableList<Shoe>>()
+    val shoeList : LiveData<MutableList<Shoe>>
+        get() = _shoeList
+
+    private fun addShoe(shoe: Shoe) {
+        _shoeList.value?.add(shoe)
+    }
+
+    init {
+        _shoeList.value = mutableListOf(
+            Shoe(name = "Nike Air Max", size = 10.5, company = "Nike", description = "New Shoes"),
+            Shoe(name = "Nike Air Force 1", size = 10.0, company = "Nike", description = "New Shoes"),
+            Shoe(name = "Nike React", size = 11.0, company = "Nike", description = "New Shoes"),
+            Shoe(name = "Nike Tiempo R9", size = 9.5, company = "Nike", description = "New Shoes"),
+            Shoe(name = "Nike UpTempo", size = 8.5, company = "Nike", description = "New Shoes"),
+            Shoe(name = "Nike Classic", size = 7.5, company = "Nike", description = "New Shoes")
+        )
+    }
+}
